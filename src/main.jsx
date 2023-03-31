7ee033b9e6c1694268b4d993eb40cfbc08f268b1
Kalash Sharma <kalash.strt@gmail.com>
2023-03-31 23:29:24 +0530

    add: tailwind and material ui support

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// importing css
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// importing libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// importing components
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
